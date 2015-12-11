9eb05a19c804d18297fecdfc9db70192edd9e7c0
Andreas Kaubisch <andreas.kaubisch@gmail.com>
2015-12-11 21:39:23 +0100

    fix review issues

--- app/src/main/java/de/kaubisch/movietheatre/Movie.java
+++ app/src/main/java/de/kaubisch/movietheatre/Movie.java
@@ -1,14 +1,40 @@
 package de.kaubisch.movietheatre;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.io.Serializable;
 
 /**
  * Created by kaubisch on 09.12.15.
  */
-public class Movie implements Serializable {
+public class Movie implements Parcelable {
     public int id;
     public String name;
     public double rating;
     public String image;
 
+    public Movie() {
+        super();
+    }
+
+    public Movie(final Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+        rating = in.readDouble();
+        image = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+        dest.writeDouble(rating);
+        dest.writeString(image);
+    }
 }
--- app/src/main/java/de/kaubisch/movietheatre/MovieDetail.java
+++ app/src/main/java/de/kaubisch/movietheatre/MovieDetail.java
@@ -1,15 +1,47 @@
 package de.kaubisch.movietheatre;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
 import java.util.Date;
 
 /**
  * Created by kaubisch on 10.12.15.
  */
-public class MovieDetail {
+public class MovieDetail implements Parcelable {
     public String title;
     public Date releaseDate;
     public double voteAverage;
     public String description;
     public String imagePath;
     public int durationInMin;
+
+    public MovieDetail() {
+        super();
+    }
+
+    public MovieDetail(final Parcel in) {
+        title = in.readString();
+        releaseDate = new Date(in.readLong());
+        voteAverage = in.readDouble();
+        description = in.readString();
+        imagePath = in.readString();
+        durationInMin = in.readInt();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeLong(releaseDate.getTime());
+        dest.writeDouble(voteAverage);
+        dest.writeString(description);
+        dest.writeString(imagePath);
+        dest.writeInt(durationInMin);
+    }
 }
